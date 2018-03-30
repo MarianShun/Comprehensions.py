8e6420f0ca24d522e1a0be9426e84899f7a713c8
MarianShun <37655860+MarianShun@users.noreply.github.com>
2018-03-30 22:59:55 +0300

    Create Comprehensions.py

---
+++ Comprehensions.py
@@ -0,0 +1,12 @@
+import sys
+
+C = sys.argv[1:]
+
+def spaces(i):
+    return ' '*(7-len(i))
+
+for i in C:
+    if '/'<i<':':
+        print(spaces(i), i, ':', float(i)*9/5+32)
+    else:
+        print(spaces(i), i, ':', 'Not a number')
